e6e94e566995c661e9a47ee36cf93062928a2536
senafathoni2998 <senaahmad2998@gmail.com>
2026-01-02 03:10:57 +0700

    refactor: restructure app initialization and error handling

--- app.ts
+++ app.ts
@@ -22,3 +22,22 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
   next();
 });
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+mongoose
+  // .connect(process.env.MONGODB_URI)
+  .connect(
+    `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=${process.env.DB_AUTH_SOURCE}`
+  )
+  .then(() => {
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  })
+  .catch((err: Error) => {
+    console.error("Database connection failed:", err);
+  });
